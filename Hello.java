ae3af51f843c91a4089dcfd9eb9437132cb5aebc
Rakesh Rathi <rathirakesh7@gmail.com>
2019-12-30 17:44:22 +0530

    Added online

--- Hello.java
+++ Hello.java
@@ -5,5 +5,6 @@ public static void main(String args[])
 System.out.println("Hello World");
 System.out.println("newly added content");
 System.out.println("To check with commit not with push and commit");
+  System.out.prinln("Added online pull it");
 }
 }
